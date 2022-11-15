e9fe62cbd4f1ba80b9514d3863f881ca89375dca
hassan <hassan@pop-os.localdomain>
2022-11-15 18:37:05 +0300

    makdir

---
+++ Data-Structures/assessment-1.py
@@ -0,0 +1,7 @@
+#What is the value of the expression 4 * (6 + 5)
+ # Answer 44
+#What is the value of the expression 4 * 6 + 5 
+# Answer 29
+#What is the value of the expression 4 + 6 * 5 
+# Answer 34
+
