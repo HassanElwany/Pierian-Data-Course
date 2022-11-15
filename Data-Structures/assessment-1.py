e752b71b0596ae57b3b3909f243982116f4e165a
hassan <hassan@pop-os.localdomain>
2022-11-15 20:31:19 +0300

    assessment

--- Data-Structures/assessment-1.py
+++ Data-Structures/assessment-1.py
@@ -5,3 +5,26 @@
 #What is the value of the expression 4 + 6 * 5 
 # Answer 34
 
+print(type(3+1.5+4))
+
+# What would you use to find a number’s square root, as well as its square?
+# ^ 
+
+s = 'hello'
+# Print out 'e' using indexing
+print(s[1])
+# Reverse the string using slicing
+print(s[::-1])
+
+# Print out the 'o'
+print(s[4])
+print(s[-1])
+
+list3 = [1,2,[3,4,'hello']]
+list3[2][2] = 'goodbye'
+print(list3)
+
+d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
+print(d['k1'][2]['k2'][1]['tough'][2][0])
+
+
